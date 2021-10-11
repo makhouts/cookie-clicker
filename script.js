3ae0daa6798bb51cb21c7496638e2851f019e1ec
OneMadness <magamag268@gmail.com>
2021-10-11 16:42:55 +0200

    Adding some mode functions

--- script.js
+++ script.js
@@ -1,48 +1,78 @@
 const cookie = document.getElementById("cookie");
 const cursorMode = document.getElementById("cursor");
 const grandmaMode = document.getElementById("grandma");
+const grandmaProd = document.getElementById("grandmaProd");
 const farmMode = document.getElementById("farm");
+const farmProd = document.getElementById("farmProd");
 const result = document.getElementById("cookieCounter");
+const audio = document.querySelector("#audio");
 
 let mode = "cursor";
-const audio = document.querySelector("#audio");
+let spells = {
+  grandma: 5,
+  farm: 2,
+};
 let count = 0;
 
 cookie.addEventListener("click", () => {
-  result.classList.add("animate__headShake");
-  cookie.classList.add("click-cookie");
-  audio.play();
-  counter(1);
+  animations(mode);
+  counter();
 });
 
 cursorMode.addEventListener("click", () => {
-  setMode("cursor");
+  mode = "cursor";
 });
 
 grandmaMode.addEventListener("click", () => {
-  setMode("grandma");
+  if (spells.grandma > 0) {
+    spells.grandma--;
+    grandmaProd.innerText = `Grandma: ${spells.grandma}`;
+    grandmaMode.classList.add("activated");
+    mode = "grandma";
+  } else {
+    alert("The grandma spells are over!");
+  }
 });
 
 farmMode.addEventListener("click", () => {
-  setMode("farm");
+  if (spells.farm > 0) {
+    spells.farm--;
+    farmProd.innerText = `Farm: ${spells.farm}`;
+    farmMode.classList.add("activated");
+    mode = "farm";
+  } else {
+    alert("The farm spells are over!");
+  }
 });
 
-function counter(value) {
-  count = count + value;
-  result.innerHTML = `${count} cookies`;
+function animations() {
+  result.classList.add("animate__headShake");
+  cookie.classList.add("click-cookie");
+}
+
+function counter(option = "cursor") {
+  mode = option;
+  if (mode == "cursor") {
+    count++;
+  } else if (mode == "grandma") {
+    const grandma = setInterval(() => {
+      count++;
+      showResult();
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(grandma);
+    }, 320000);
+  } else if (mode == "farm") {
+    counter = counter + 5;
+  }
+  showResult();
   setTimeout(() => {
     result.classList.remove("animate__headShake");
     cookie.classList.remove("click-cookie");
   }, 200);
 }
 
-function setMode(mode) {
-  mode = mode;
-  if (mode == "cursor") {
-    console.log(mode);
-  } else if (mode == "grandma") {
-    console.log(mode);
-  } else {
-    console.log(mode);
-  }
+function showResult() {
+  audio.play();
+  result.innerHTML = `${count} cookies`;
 }
