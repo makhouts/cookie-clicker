c77f5b4b5633a89cd86a6293e7982b1d31d88548
Sofiane Makhout <makhouts@gmail.com>
2021-10-11 14:39:17 +0200

    small js change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
 const result = document.getElementById("RESULT_ID");
 let count = 0;
 
-let status = `${count} cookies`
+let status = `${count} cookies`;
 
 incrementBtn.addEventListener("click", () => {
   counter(1);
@@ -17,4 +17,3 @@ function counter(value) {
   count = count + value;
   result.innerHTML = `Count: ${count}`;
 }
-console.log('testg')
