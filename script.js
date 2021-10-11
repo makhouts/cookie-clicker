20d28432a5f20b77aad470e9ca9da09792a92f79
Sofiane Makhout <makhouts@gmail.com>
2021-10-11 15:11:46 +0200

    test

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-<<<<<<< HEAD
-const incrementBtn = document.getElementById("cookie");
-const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
-const result = document.getElementById("RESULT_ID");
-let count = 0;
-
-let status = `${count} cookies`
-
-incrementBtn.addEventListener("click", () => {
-  counter(1);
-});
-
-multiplyBtn.addEventListener("click", () => {
-  counter(2);
-});
-
-function counter(value) {
-  count = count + value;
-  result.innerHTML = `Count: ${count}`;
-}
-=======
 const cookie = document.getElementById("cookie");
 // const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
 const result = document.getElementById("cookieCounter");
 let count = 0;
 
-<<<<<<< HEAD
-let status = `${count} cookies`;
-
-incrementBtn.addEventListener("click", () => {
-=======
 cookie.addEventListener("click", () => {
   cookie.classList.add("click-cookie");
->>>>>>> 0146aec71d318dea3aeb55920454d9d02c6a515d
+  console.log('s')
   counter(1);
 });
 
@@ -46,8 +20,3 @@ function counter(value) {
     cookie.classList.remove("click-cookie");
   }, 200);
 }
-<<<<<<< HEAD
-=======
-console.log("testg");
->>>>>>> 0146aec71d318dea3aeb55920454d9d02c6a515d
->>>>>>> main
