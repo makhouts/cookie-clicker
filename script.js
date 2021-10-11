5c8c3cda80101bb5678a4fd7e79a82c7185dc513
Sofiane Makhout <makhouts@gmail.com>
2021-10-11 11:34:30 +0200

    added styles

---
+++ script.js
@@ -0,0 +1,19 @@
+const incrementBtn = document.getElementById("cookie");
+const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
+const result = document.getElementById("RESULT_ID");
+let count = 0;
+
+let status = `${count} cookies`
+
+incrementBtn.addEventListener("click", () => {
+  counter(1);
+});
+
+multiplyBtn.addEventListener("click", () => {
+  counter(2);
+});
+
+function counter(value) {
+  count = count + value;
+  result.innerHTML = `Count: ${count}`;
+}
