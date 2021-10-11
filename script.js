688ef1a587f71bde2fd245f82daa1e31da766bf6
OneMadness <magamag268@gmail.com>
2021-10-11 15:25:04 +0200

    Adding logic

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const cookie = document.getElementById("cookie");
-// const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
+const cursorMode = document.getElementById("cursor");
+const grandmaMode = document.getElementById("grandma");
+const farmMode = document.getElementById("farm");
 const result = document.getElementById("cookieCounter");
+
+let mode = "cursor";
 let count = 0;
 
 cookie.addEventListener("click", () => {
+  result.classList.add("animate__headShake");
   cookie.classList.add("click-cookie");
   counter(1);
 });
 
-// multiplyBtn.addEventListener("click", () => {
-//   counter(2);
-// });
+cursorMode.addEventListener("click", () => {
+  setMode("cursor");
+});
+
+grandmaMode.addEventListener("click", () => {
+  setMode("grandma");
+});
+
+farmMode.addEventListener("click", () => {
+  setMode("farm");
+});
 
 function counter(value) {
   count = count + value;
   result.innerHTML = `${count} cookies`;
   setTimeout(() => {
+    result.classList.remove("animate__headShake");
     cookie.classList.remove("click-cookie");
   }, 200);
 }
-console.log("testg");
+
+function setMode(mode) {
+  mode = mode;
+  if (mode == "cursor") {
+    console.log(mode);
+  } else if (mode == "grandma") {
+    console.log(mode);
+  } else {
+    console.log(mode);
+  }
+}
