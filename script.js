486417305bbecdd8eb9978ea3b731178bee03991
OneMadness <magamag268@gmail.com>
2021-10-11 11:04:21 +0200

    Some html changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const incrementBtn = document.getElementById("INCREMENT_BTN_ID");
+const cookie = document.getElementById("cookie");
 const multiplyBtn = document.getElementById("MULTIPLY_BTN_ID");
-const result = document.getElementById("RESULT_ID");
+const result = document.getElementById("cookieCounter");
 let count = 0;
 
-incrementBtn.addEventListener("click", () => {
+cookie.addEventListener("click", () => {
   counter(1);
 });
 
-multiplyBtn.addEventListener("click", () => {
-  counter(2);
-});
+// multiplyBtn.addEventListener("click", () => {
+//   counter(2);
+// });
 
 function counter(value) {
   count = count + value;
-  result.innerHTML = `Count: ${count}`;
+  result.innerHTML = `${count} cookies`;
 }
